6da3d70a6b690e2ee9226634b5d3b47ef06f494b
rahulatoz365-del <rahulrogerxd111@gmail.com>
2025-12-26 15:32:35 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -17,9 +17,6 @@ const shopReviewRouter = require("./routes/shop/review-routes");
 
 const commonFeatureRouter = require("./routes/common/feature-routes");
 
-//create a database connection -> u can also
-//create a separate file for this and then import/use that file here
-
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB connected"))
